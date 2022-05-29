d69548d90827dfa141af0fb6f28bc1e0f2b7b079
oeladawy <omnia1699@gmail.com>
2022-05-29 12:49:29 +0200

    init commit

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+const messages = [];
+
+app.use(express.json());
+app.use(cors());
+
+app.post('/messages' , (req,res)=>{
+    const {body} = req;
+    messages.push(body);
+    console.log(body);
+    res.status(204).end;
+})
+
+app.get('/messages' , (req,res)=>{
+    res.json(messages);
+})
+
+/////long polling
+const sub = {};
+
+app.get('/longMessages' , (req,res)=>{
+    const id = Math.ceil(Math.random()* 10000);
+    sub[id] = res; ////hang req
+
+})
+
+app.post('/longMessages' , (req,res)=>{
+    const {body} = req;
+    Object.entries(sub).forEach(([id,response])=>{
+        response.json(body);
+        delete sub[id];
+    });
+    res.status(204).end();
+})
+
+app.listen(3000,()=>{
+    console.log("app running");
+}) 
---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Longpolling from './Longpolling';
+//import Shortpolling from './Shortpolling';
+
+
+
+function App() {
+  return (
+    <>
+   <Longpolling />
+    </>
+  );
+}
+
+export default App;
